42fe0653b55ef557b77b757817f9948cb7337fcf
Henry Yuan <henry.yuan@ebatte.com>
2024-02-05 11:55:04 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ethers } from 'ethers';
 import { useSigner, useAccount, useProvider } from 'wagmi';
 import React, { useState, useEffect } from 'react';
 import { Theme, SwapWidget } from '@uniswap/widgets';
+import type { Web3Provider } from '@ethersproject/providers'
 import { JsonRpcSigner } from '@ethersproject/providers';
 import { EventEmitter } from 'events';
 import '@uniswap/widgets/fonts.css';
@@ -15,15 +16,54 @@ function App() {
   const [defaultOutputTokenAddress, setDefaultOutputTokenAddress] = useState('0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889');
   const provider = useProvider();
   const executeSigner = useSigner<JsonRpcSigner>().data;
-  const executeProvider = executeSigner ? executeSigner.provider as ethers.providers.JsonRpcProvider : undefined;  
+  const [executeProvider, setExecuteProvider] = useState<ethers.providers.JsonRpcProvider | null>(null);
+  const jsonRpcUrlMap = {
+    1: [`https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`], // Ethereum Mainnet
+    137: [`https://polygon-mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`], // Polygon Mainnet
+    80001: [`https://polygon-mumbai.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`] // Polygon Mumbai Testnet
+  }
+
+  useEffect(() => {
+    let myprovider = executeSigner 
+      ? executeSigner.provider as ethers.providers.JsonRpcProvider 
+      : provider;
+      setExecuteProvider(myprovider);
+  }, [executeSigner]);
+
+  // if (executeProvider) {
+  //   executeProvider = new Proxy(executeProvider, {
+  //     get: function(target: any, propKey: any) {
+  //       const originalMethod = target[propKey];
+  //       if (typeof originalMethod === 'function') {
+  //         return (...args: any) => {
+  //           console.log(`${propKey}, params:`, args);
+  //           const result = originalMethod.apply(target, args);
+  //           if (propKey === 'getSigner' && result) {
+  //             for (const key in result) {
+  //               if (typeof result[key] === 'function') {
+  //                 result[key] = new Proxy(result[key], {
+  //                   apply: function(target, thisArg, argumentsList) {
+  //                     console.log(`${key}, params:`, argumentsList);
+  //                     return Reflect.apply(target, thisArg, argumentsList);
+  //                   }
+  //                 });
+  //               }
+  //             }
+  //           }
+  //           return result;
+  //         };
+  //       }
+  //       return originalMethod;
+  //     }
+  //   });
+  // }
   /*
   When using wagmi@v0.11, useProvider() will return the fallback provider, not the wallet-connected provider. 
   You should instead pass useSigner<JsonRpcSigner>().data?.provider to reflect the user's connected wallet.
   */
-
-  eventEmitter.on('uniswapSendTransaction', (transactionParams) => {
-    console.log('Received uniswapSendTransaction event with params:', transactionParams);
-  });
+  // eventEmitter.on('uniswapSendTransaction', (transactionParams) => {
+  //   console.log('Received uniswapSendTransaction event with params:', transactionParams);
+  // });
 
   useEffect(() => {
     if (provider) {
@@ -72,7 +112,23 @@ function App() {
         {/* Main Content */}
         <div className="flex-grow p-4 flex justify-center mt-4">
           <div className="Uniswap">
-            <SwapWidget provider={executeProvider} theme={theme} defaultInputTokenAddress={defaultInputTokenAddress} defaultOutputTokenAddress={defaultOutputTokenAddress} defaultInputAmount={0.001} />
+            <SwapWidget 
+            provider={executeProvider} 
+            theme={theme} 
+            defaultInputTokenAddress={defaultInputTokenAddress} 
+            defaultOutputTokenAddress={defaultOutputTokenAddress} 
+            defaultInputAmount={0.001}
+            jsonRpcUrlMap={jsonRpcUrlMap}
+            onConnectWalletClick={() => {
+              console.log('Connect wallet button clicked');
+              return Promise.resolve(false);
+            }}
+            onReviewSwapClick={() => {
+              console.log('Review swap button clicked');
+              return Promise.resolve(true);
+            }}
+            hideConnectionUI={true}
+            />
           </div>
         </div>
         {/* Main Content end */}
